42dff8d1bb0ba88071284c1d33856f3b48320b36
Khoirunnisa02 <92660879+Khoirunnisa02@users.noreply.github.com>
2022-10-26 22:52:00 +0700

    Add files via upload

---
+++ PersonAntoriko.java
@@ -0,0 +1,27 @@
+public class PersonAntoriko{
+    public static void main(String[] args) {
+    
+    Person Anton  = new Person ();
+    
+    /* memanggil atribut dan memberi nilai */
+    
+    Anton.setNama("Anton");
+    Anton.setJenisKelamin("Pria");
+    Anton.setUmur(25);
+    System.out.println("Nama         : " + Anton.getNama());
+    System.out.println("JenisKelamin : " + Anton.getJenisKelamin());
+    System.out.println("Umur         : " + Anton.getUmur());
+    System.out.println("----------------------");
+    
+    Person Riko = new Person ();
+    Riko.setNama("Riko");
+    Riko.setJenisKelamin("Pria");
+    Riko.setUmur(29);
+    System.out.println("Nama         : " + Riko.getNama());
+    System.out.println("JenisKelamin : " + Riko.getJenisKelamin());
+    System.out.println("Umur         : " + Riko.getUmur());
+    
+    
+    
+    }
+    }
